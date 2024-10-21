dd13db2edc88e321526dde9796947cdf0c8132ce
andrewdanieltodd@gmail.com <andrewdanieltodd@gmail.com>
2024-10-21 14:46:37 -0600

    update to interface scoping. Added namespace

--- include/RendererInterface.h
+++ include/RendererInterface.h
@@ -1,15 +1,18 @@
 #ifndef RENDERER_INTERFACE_H
 #define RENDERER_INTERFACE_H
 
-class RendererInterface
+namespace UltReality::Rendering
 {
-public:
-	virtual ~RendererInterface() = default;
+	class RendererInterface
+	{
+	public:
+		virtual ~RendererInterface() = default;
 
-	virtual bool Initialize() = 0;
-	virtual void CreateBuffer() = 0;
-	virtual void Render() = 0;
-	virtual void Present() = 0;
-};
+		virtual bool Initialize() = 0;
+		virtual void CreateBuffer() = 0;
+		virtual void Render() = 0;
+		virtual void Present() = 0;
+	};
+}
 
 #endif //!RENDERER_INTERFACE_H
