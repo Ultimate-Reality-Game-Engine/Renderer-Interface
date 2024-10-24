468bc5a9f42f476057084b02c9913a9ee4ed2d36
andrewdanieltodd@gmail.com <andrewdanieltodd@gmail.com>
2024-10-23 22:46:55 -0600

    update to interface and Package config

--- include/RendererInterface.h
+++ include/RendererInterface.h
@@ -1,5 +1,31 @@
-#ifndef RENDERER_INTERFACE_H
-#define RENDERER_INTERFACE_H
+#ifndef ULTREALITY_RENDERING_RENDERER_INTERFACE_H
+#define ULTREALITY_RENDERING_RENDERER_INTERFACE_H
+
+#if defined(_WIN_TARGET)
+	#if defined(RENDERER_INTERFACE_EXPORTS)
+		#define RENDERER_INTERFACE_API __declspec(dllexport)
+	#else
+		#define RENDERER_INTERFACE_API __declspec(dllimport)
+	#endif
+
+	#define RENDERER_INTERFACE_CALL __cdecl
+#elif defined(_linux)
+	#if defined(RENDER_INTERFACE_EXPORTS)
+		#define RENDERER_INTERFACE_API __attribute__((visibility("default")))
+	#else
+		#define RENDERER_INTERFACE_API __attribute__((visibility("default")))
+	#endif
+
+	#define RENDERER_INTERFACE_CALL __cdecl
+#elif(_mac)
+	#if defined(RENDER_INTERFACE_EXPORTS)
+		#define RENDERER_INTERFACE_API __attribute__((visibility("default")))
+	#else
+		#define RENDERER_INTERFACE_API __attribute__((visibility("default")))
+	#endif
+
+	#define RENDERER_INTERFACE_CALL __cdecl
+#endif
 
 namespace UltReality::Rendering
 {
@@ -8,11 +34,11 @@ namespace UltReality::Rendering
 	public:
 		virtual ~RendererInterface() = default;
 
-		virtual bool Initialize() = 0;
-		virtual void CreateBuffer() = 0;
-		virtual void Render() = 0;
-		virtual void Present() = 0;
+		virtual bool RENDERER_INTERFACE_CALL Initialize() = 0;
+		virtual void RENDERER_INTERFACE_CALL CreateBuffer() = 0;
+		virtual void RENDERER_INTERFACE_CALL Render() = 0;
+		virtual void RENDERER_INTERFACE_CALL Present() = 0;
 	};
 }
 
-#endif //!RENDERER_INTERFACE_H
+#endif //!ULTREALITY_RENDERING_RENDERER_INTERFACE_H
